5cb3a5b0fed3a3a3a8e139627a03b4ead87fac88
npignier <npignier@88c683e2-3181-9a4f-a146-acc1575aec5e>
2011-11-16 14:18:57 +0000

    - Montée de version du generateur - Possibilité de générer plusieurs procédures en une passe
    git-svn-id: svn://bmmery19/DomaineIHMs/Webservices/tools/wrapper-generator-maven-plugin/trunk@6787 88c683e2-3181-9a4f-a146-acc1575aec5e

--- src/main/java/com/geodisbm/ws/tools/maven/pwg/PSWrapperGeneratorMojo.java
+++ src/main/java/com/geodisbm/ws/tools/maven/pwg/PSWrapperGeneratorMojo.java
@@ -17,54 +17,54 @@ package com.geodisbm.ws.tools.maven.pwg;
  */
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.maven.model.FileSet;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.codehaus.plexus.util.FileUtils;
 
 import com.geodisbm.ws.tools.generator.Marshaller;
 import com.geodisbm.ws.tools.generator.PSGenerator;
-import com.geodisbm.ws.tools.generator.ParameterContainer;
 import com.geodisbm.ws.tools.generator.StoredProcedure;
 
 /**
- * Goal which generates java wrapper around a Stored Procedure based on a description file.
+ * Goal which generates java wrapper around a Stored Procedure based on a
+ * description file.
  * 
- * @goal generate-ps-wrapper
+ * @goal generate-procedure-wrapper
  * 
  */
 public class PSWrapperGeneratorMojo extends AbstractMojo {
-  
+
   private static final String PS = System.getProperty("file.separator");
 
   /**
    * Location of the description file
-   * @parameter expression="${basedir}/src/main/resources"
+   * 
+   * @parameter expression="${ps-wrapper-generator.sourceFiles}"
    */
-  private File inputDescriptionFile;
-  
+  private FileSet sourceFiles;
+
   /**
    * Location of the outputDirectory
    * 
-   * @parameter expression="${basedir}/src/main/java"
+   * @parameter expression="${ps-wrapper-generator.outputDirectory}"
+   *            default-value="${basedir}/src/main/java"
    * @required
    */
   private File outputDirectory;
-  
-  /**
-   * PGM Name
-   * @parameter
-   * @required
-   */
-  private String psName;
-  
+
   /**
    * Package name
    * 
-   * @parameter default-value="com.geodisbm.ws.gen"
+   * @parameter expression="${ps-wrapper-generator.packageName}"
+   *            default-value="com.geodisbm.ws.gen"
    * @required
    */
   private String packageName;
-  
 
   public void execute() throws MojoExecutionException {
     getLog().info("****************************************");
@@ -73,24 +73,62 @@ public class PSWrapperGeneratorMojo extends AbstractMojo {
     generateSPWrapper();
   }
 
-  private void generateSPWrapper() {
-    String absolutePath = inputDescriptionFile.getAbsolutePath();
-    getLog().info("Reading input file metadatas.xml from " + absolutePath + "...");
+  @SuppressWarnings("unchecked")
+  private void generateSPWrapper() throws MojoExecutionException {
+    File baseDir = null;
+    try {
+      baseDir = new File(this.sourceFiles.getDirectory());
+    }
+    catch (Exception e) {
+      throw new MojoExecutionException(this.sourceFiles.getDirectory() + " is not a valid path");
+    }
+    if (baseDir != null) {
+      List<File> files = null;
+      try {
+        files = FileUtils.getFiles(baseDir, getCommaSeparatedList(this.sourceFiles.getIncludes()),
+            getCommaSeparatedList(this.sourceFiles.getExcludes()));
+        getLog().debug("Files to process : " + files.toString());
+      }
+      catch (IOException e) {
+        getLog().warn("No source file to process");
+      }
+      for (final File f : files) {
+        getLog().info("Processing " + f);
+        processPSInputFile(f);
+      }
+    }
+  }
+
+  private void processPSInputFile(File f) {
+    String absolutePath = f.getParent();
+    getLog().info("Reading input file from " + absolutePath + "...");
     getLog().debug("file exists ? : " + (new File(absolutePath)).exists());
     Marshaller m = new Marshaller();
-    StoredProcedure unmarshall = m.unmarshallStoredProcedure(absolutePath);
-    getLog().debug(unmarshall.toString());
+    StoredProcedure storedProcedure = m.unmarshallStoredProcedure(absolutePath, f.getName());
+    getLog().debug(storedProcedure.toString());
     getLog().info("Reading done");
-    
     PSGenerator g = new PSGenerator();
-    g.setObject(unmarshall);
-    String outputFolder = outputDirectory.getAbsolutePath() + PS + packageName.replaceAll("\\.", "\\" + PS) + PS + psName;
+    g.setObject(storedProcedure);
+    String outputFolder = outputDirectory.getAbsolutePath() + PS + packageName.replaceAll("\\.", "\\" + PS) + PS
+        + storedProcedure.getName();
     getLog().debug("Generation root dir : " + outputFolder);
     g.setOutputFolder(outputFolder);
     getLog().info("Generating wrapper...");
     g.instantiateAll();
     getLog().info("Generating done");
-    
+  }
+
+  protected String getCommaSeparatedList(final List<String> list) {
+    final StringBuffer buffer = new StringBuffer();
+    final Iterator<String> it = list.iterator();
+    while (it.hasNext()) {
+      Object object = it.next();
+      buffer.append(object.toString());
+      if (it.hasNext()) {
+        buffer.append(",");
+      }
+    }
+    return buffer.toString();
   }
 
 }
